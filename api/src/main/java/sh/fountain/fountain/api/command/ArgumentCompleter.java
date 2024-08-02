42bcac306a9ee32323fa67f83127260e3e896eb2
Alexander Linder <seine@eloque.nz>
2024-08-02 16:42:53 +0200

    Fix documentation typos

--- api/src/main/java/sh/fountain/fountain/api/command/ArgumentCompleter.java
+++ api/src/main/java/sh/fountain/fountain/api/command/ArgumentCompleter.java
@@ -5,20 +5,20 @@ import org.bukkit.command.CommandSender;
 import java.util.List;
 
 /**
- * Represents a supplier of all possible valid values for a commands argument
- * <p>An ArgumentCompleter is assigned to a command handlers argument via the
+ * Represents a supplier of all possible valid values for a command's argument
+ * <p>An ArgumentCompleter is assigned to a command handler's argument via the
  * {@link AutoComplete}
- * annotation. And is invoked any time tabcompletion for the corresponding argument is requested.
+ * annotation. And is invoked any time tab-completion for the corresponding argument is requested.
  * </p>
  */
 @FunctionalInterface
 public interface ArgumentCompleter {
     /**
-     * Returns all valid values for this completers corresponding argument
+     * Returns all valid values for this completer's corresponding argument
      *
-     * @param argsBefore the values of arguments that occur before this completers corresponding argument
-     * @param sender the user tabcompletion is being performed for
-     * @return All valid values for this completers corresponding argument
+     * @param argsBefore the values of arguments that occur before this completer's corresponding argument
+     * @param sender the user tab-completion is being performed for
+     * @return All valid values for this completer's corresponding argument
      */
     List<String> possibilities(List<String> argsBefore, CommandSender sender);
 }
