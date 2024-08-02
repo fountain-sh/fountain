588975d6f3a9cc0bb753fa3f6c7b7e9c1a6c5939
Alexander Linder <seine@eloque.nz>
2024-08-02 16:54:34 +0200

    Add documentation to metadata annotations

--- api/src/main/java/sh/fountain/fountain/api/plugin/Permissions.java
+++ api/src/main/java/sh/fountain/fountain/api/plugin/Permissions.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Containing annotation type for multiple {@link Permission} annotations
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Permissions {
