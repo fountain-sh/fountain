7d386c5920ab8e6d65fd8f1c40e3d9730b54b497
Alexander Linder <seine@eloque.nz>
2024-08-02 16:47:13 +0200

    Add documentation to lifecycle interfaces

--- api/src/main/java/sh/fountain/fountain/api/plugin_actions/PluginLoadAction.java
+++ api/src/main/java/sh/fountain/fountain/api/plugin_actions/PluginLoadAction.java
@@ -1,5 +1,16 @@
 package sh.fountain.fountain.api.plugin_actions;
 
+/**
+ * Defines an action to be run when the plugin is loaded by Paper.
+ *
+ * @see <a href="https://docs.papermc.io/paper/dev/how-do-plugins-work#plugin-lifecycle">Paper Plugin Lifecycle</a>
+ */
 public interface PluginLoadAction {
+
+    /**
+     * Method run when the plugin is loaded
+     *
+     * @throws Exception wraps any exception thrown by contained code
+     */
     void onPluginLoad() throws Exception;
 }
