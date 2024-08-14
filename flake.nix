40cc1c705a5463750cc508b9bb658305ea328fe2
Joshua Collings <me@cojo.sh>
2024-08-14 16:22:14 +0200

    Add nix flake to describe toolchain and use it in ci

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Fountain Toolchain";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=24.05";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+          devShells.default = mkShell {
+          buildInputs = [ jdk17 gradle ];
+        };
+      }
+  );
+}
